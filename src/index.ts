3859c956abeb984034fbff8d8706458f92b7e6ca
Kin Leon Zinzombe <kinzinzombe07@gmail.com>
2025-07-16 19:09:16 +0200

    [ADD]: Cors middleware

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,15 @@ import { Hono } from 'hono'
 import getConfessions from './controllers/confessions/getConfessions'
 import createConfession from './controllers/confessions/createConfession';
 import { logger } from "hono/logger";
+import { cors } from "hono/cors";
 
 const app = new Hono()
 
 // middleware
 app.use("*", logger())
+app.use("*", cors({
+    origin: ["http://localhost:3000", "https://openconfess.buzz"]
+}))
 
 // Routes
 app.post("/api/openconfess/confessions", createConfession);
