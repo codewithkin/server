f1161d4914bd79ae386095c09ace5910eabb06e4
Kin Leon Zinzombe <kinzinzombe07@gmail.com>
2025-07-16 18:56:00 +0200

    [ADD]: Hono logger for requests

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
 import { Hono } from 'hono'
 import getConfessions from './controllers/confessions/getConfessions'
 import createConfession from './controllers/confessions/createConfession';
+import { logger } from "hono/logger";
 
 const app = new Hono()
+
+// middleware
+app.use("*", logger())
 
 // Routes
 app.post("/api/open/confessions", createConfession);
