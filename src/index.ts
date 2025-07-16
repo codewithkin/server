0af235c7262c6bad6f941b881b09cf4ef3dbc5d6
Kin Leon Zinzombe <kinzinzombe07@gmail.com>
2025-07-16 23:26:54 +0200

    Minor changes

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import { logger } from "hono/logger";
 import { cors } from "hono/cors";
 import likeConfession from './controllers/confessions/likeConfession';
 import unlikeConfession from './controllers/confessions/unlikeConfession';
+import createComment from './controllers/comments/createComment';
 
 const app = new Hono()
 
@@ -19,5 +20,6 @@ app.post("/api/openconfess/confessions", createConfession);
 app.get("/api/openconfess/confessions", getConfessions);
 app.post("/api/openconfess/confessions/like", likeConfession);
 app.delete("/api/openconfess/confessions/like", unlikeConfession);
+app.post("/api/openconfess/confessions/comments", createComment);
 
 export default app
