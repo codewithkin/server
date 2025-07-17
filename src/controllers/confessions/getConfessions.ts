e457326825d9b556002ca5b6ee7fb1d75fd4d9e3
Kin Leon Zinzombe <kinzinzombe07@gmail.com>
2025-07-17 12:40:18 +0200

    [ADD]: Filter logic

--- src/controllers/confessions/getConfessions.ts
+++ src/controllers/confessions/getConfessions.ts
@@ -3,9 +3,48 @@ import prisma from "../../helpers/prisma";
 
 export default async function getConfessions(c: Context) {
     try {
+        const { filter, date } = c.req.query()
+
+        if (filter === "All") {
+            // Get the confessions
+            const confessions = await prisma.confession.findMany({
+                include: {
+                    likes: true,
+                    comments: true
+                },
+                orderBy: {
+                    postedAt: "asc"
+                }
+            });
+
+            return c.json(confessions);
+        } else if (filter === "Trending") {
+
+            // Get the confessions
+            const confessions = await prisma.confession.findMany({
+                include: {
+                    likes: true,
+                    comments: true
+                }
+            });
+
+            return c.json(confessions);
+        }
+
+        // Logic for "Today" confessions
+
+        const today = new Date(date);
+
+        const tomorrow = new Date(today.getDate() + 1);
 
         // Get the confessions
         const confessions = await prisma.confession.findMany({
+            where: {
+                postedAt: {
+                    gte: today,
+                    lte: tomorrow
+                }
+            },
             include: {
                 likes: true,
                 comments: true
